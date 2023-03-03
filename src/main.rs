9ae3c42d10e61f4df6f8253b7dd76efa61d9a463
Toby Devlin <toby@tobydevlin.com>
2023-03-03 12:02:44 +1100

    move to new framework

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,93 @@
-extern crate clap;
-extern crate chrono;
-extern crate csv;
-extern crate serde;
-extern crate ansi_term;
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
-use clap::{App, Arg, crate_version, crate_authors, crate_description};
-use chrono::{Local, DateTime};
+use clap::{Parser, Subcommand};
+use config::{Config, ConfigBuilder, File, FileFormat};
+use dirs::{data_dir, home_dir};
 
-use crate::note::Note;
-use std::path::Path;
-use std::fs::{File, OpenOptions};
-use csv::Error;
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config_file: Option<PathBuf>,
 
-mod note;
-mod setup;
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
 
-/* CLI Commands */
-const TAKE_COMMAND: &str = "take";
-const TAKE_ARG_MESSAGE: &str = "message";
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
-const PRINT_COMMAND: &str = "print";
+#[derive(Subcommand)]
+enum Commands {
+    /// Take a note
+    Take {
+        /// lists test values
+        #[arg(short, long)]
+        note_value: String,
+    },
 
-const SETUP_COMMAND: &str = "setup";
+    /// List your notes
+    List { count: u8 },
 
-const OUT_FILE_LOCATION: &str = "./out.csv";
+    /// Search
+    Search {
+        /// Term to search for
+        #[arg(short, long)]
+        term: String,
+    },
+}
+
+fn take_note(note_value: &String, _verbose: u8) {
+    println!("taking note {}", note_value)
+}
+
+fn list_notes(count: &u8, _verbose: u8) {
+    println!("printing {} notes", count)
+}
+
+fn search_notes(search_term: &String, _verbose: u8) {
+    println!("searching notes with term {}", search_term)
+}
 
 fn main() {
-    let enabled = ansi_term::enable_ansi_support();
-    let matches = App::new("NOTEABLE - The CLI Note App")
-        .author(crate_authors!())
-        .version(format!("v{}", crate_version!()).as_str())
-        .about(crate_description!())
-        .subcommand(App::new(SETUP_COMMAND))
-        .subcommand(App::new(TAKE_COMMAND)
-            .about("Take a new note")
-            .arg(Arg::with_name("append")
-                .short('a')
-                .long("append")
-                .about("Appends this note to any notes you've taken today.
-                  If there are 0 notes this will take a single note as default.
-                  If there are 2+ notes, this will append to the first note for the day.")
-            )
-            .arg(Arg::with_name(TAKE_ARG_MESSAGE)
-                .short('m')
-                .long("message")
-                .about("Takes a note without the prompt")
-                .takes_value(true)
-            )
-        )
-        .subcommand(App::new(PRINT_COMMAND)
-            .about("Print notes")
-        )
-        // todo: build setup comand that builds out setup for all OSs
-        // .arg(Arg::with_name(CONFIG_PATH_OPTION)
-        //     .about("The path to your config .yml file")
-        //     .long("config")
-        //     .short('c')
-        //     .default_value(CONFIG_DEFAULT_PATH)
-        // )
-        .get_matches();
+    let cli = Cli::parse();
+    let verbose = cli.verbose;
+    if verbose > 0 {
+        println!("verbose mode is on");
+    };
 
-    let f = OpenOptions::new()
-        .create(true)
-        .read(true)
-        .append(true)
-        .open(Path::new(OUT_FILE_LOCATION))
-        .expect("error opening file");
-
-    let mut csv_reader = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .comment(Some(b'#'))
-        .from_reader(
-            f.try_clone().expect("Error with file opening for read")
-        );
-
-    match matches.subcommand() {
-        (TAKE_COMMAND, Some(arg)) => {
-            let note_value: Note;
-            if arg.is_present(TAKE_ARG_MESSAGE) {
-                note_value = Note::from_string(arg.value_of(TAKE_ARG_MESSAGE).unwrap().to_string())
-            } else {
-                note_value = Note::from_prompt();
-            };
-
-            let write_header_row = match csv_reader.deserialize::<Note>().next() {
-                // if there's nothing in the file we can assume there's no headers, we should write them
-                None => true,
-                // if there is something there probably been written before and we can ignore
-                Some(_) => false
-            };
-
-            let mut writer = csv::WriterBuilder::new()
-                .has_headers(write_header_row)
-                .from_writer(
-                    f.try_clone().expect("error")
-                );
-            writer.serialize(note_value);
+    let config_path = match cli.config_file {
+        None => {
+            //use default config path
+            home_dir()
+                .map(|p| p.join("notes.txt"))
+                .expect("No data dir found!!")
         }
-        (PRINT_COMMAND, Some(arg)) => {
-            println!("print");
-            let records_iter = csv_reader.deserialize::<Note>();
-            for r in records_iter {
-                let record: Note = r.unwrap();
-                println!("{:?}", record);
-                //https://docs.rs/csv/1.1.3/csv/tutorial/index.html#setup
-            };
-        }
-        _ => {
-            println!("none")
-        }
+        Some(config_path) => config_path,
+    };
+
+    if verbose > 0 {
+        println!("Using config at {}", config_path.display());
+    }
+
+    let builder = Config::builder()
+        .set_default("default", "1")
+        .unwrap()
+        .add_source(File::from(config_path))
+        //  .add_async_source(...)
+        .set_override("override", "1")
+        .unwrap()
+        .build();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Take { note_value }) => take_note(note_value, verbose),
+        Some(Commands::List { count }) => list_notes(count, verbose),
+        Some(Commands::Search { term }) => search_notes(term, verbose),
+        _ => {}
     }
 }
