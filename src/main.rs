1596ddc69bb8b5dabd5b56f2cbe436c77fbc67c4
Toby Devlin <toby@tobydevlin.com>
2023-03-03 17:09:17 +1100

    refactor to a more nautural approach to the Note

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::cmp::min;
+use std::fmt::{Display, Formatter};
 use std::fs::{File, OpenOptions};
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufReader, Write};
 use std::path::PathBuf;
 
-use chrono::Local;
+use chrono::{DateTime, Local};
 use clap::{Parser, Subcommand};
 use dirs::home_dir;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use fuzzy_matcher::FuzzyMatcher;
 use rev_lines::RevLines;
+use scan_fmt::scan_fmt;
 
 // https://docs.rs/clap/4.1.8/clap/_derive/index.html
 #[derive(Parser)]
@@ -29,9 +34,13 @@ enum Commands {
     Take {
         /// lists test values
         note_value: String,
+
+        /// Echo the note to the console rather than write to disk
+        #[arg(short, long)]
+        echo: bool,
     },
 
-    /// List your notes. This will list notes chronologically from
+    /// List your notes.
     List {
         // Number of notes to list
         #[arg(short = 'n', long, default_value = "10")]
@@ -47,6 +56,9 @@ enum Commands {
         #[arg(short = 'n', long, default_value = "10")]
         count: u8,
     },
+
+    /// echo the notes file path
+    Path,
 }
 
 #[derive(Debug)]
@@ -72,6 +84,43 @@ impl Settings {
     }
 }
 
+struct Note {
+    timestamp: DateTime<Local>,
+    note_value: String,
+}
+
+impl Note {
+    fn new(note_value: String) -> Note {
+        return Note {
+            timestamp: Local::now(),
+            note_value,
+        };
+    }
+
+    fn new_from_line(line: &String) -> Note {
+        // This must match the fmt below, as the parse may fail.
+        let fmt_res = scan_fmt!(line, "{} - {}", String, String);
+
+        // todo: make this destructuring more solid
+        let (timestamp_str, note_value) =
+            fmt_res.expect("Format of notes should be as \"{timestamp} - {note}\"");
+        let timestamp = DateTime::parse_from_rfc2822(&*timestamp_str)
+            .expect("All Datetime should be in the rfc2822 format")
+            .with_timezone(&Local);
+
+        return Note {
+            timestamp,
+            note_value,
+        };
+    }
+}
+
+impl Display for Note {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {}", self.timestamp.to_rfc2822(), self.note_value)
+    }
+}
+
 fn get_notes_file(path: PathBuf) -> File {
     let file = match OpenOptions::new()
         .create(true)
@@ -85,13 +134,16 @@ fn get_notes_file(path: PathBuf) -> File {
     file
 }
 
-fn take_note(settings: Settings, note_value: &String) {
+fn take_note(settings: Settings, note_value: &String, echo: &bool) {
     println!("taking note {}", note_value);
     let mut file = get_notes_file(settings.notes_file_path);
 
-    let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-
-    writeln!(file, "{} - {}", timestamp, note_value).unwrap();
+    let note = Note::new(note_value.clone());
+    if *echo {
+        println!("{}", note)
+    } else {
+        writeln!(file, "{}", note).unwrap();
+    }
 }
 
 fn list_notes(settings: Settings, count: &u8) {
@@ -100,18 +152,12 @@ fn list_notes(settings: Settings, count: &u8) {
     }
 
     let file = get_notes_file(settings.notes_file_path);
-    let reader1 = BufReader::new(file);
-    let reader = RevLines::new(reader1).unwrap();
+    let reader = RevLines::new(BufReader::new(file)).unwrap();
     for (i, note) in reader.enumerate() {
         if i >= *count as usize {
             break;
         }
         match settings.verbosity {
-            // warnings only
-            0 => {
-                println!("{}", note);
-            }
-            // info level
             _ => {
                 println!("Note {}: {}", i, note);
             }
@@ -119,11 +165,46 @@ fn list_notes(settings: Settings, count: &u8) {
     }
 }
 
-fn search_notes(_settings: Settings, search_term: &String, count: &u8) {
-    println!(
-        "searching notes with term {}, printing {} notes",
-        search_term, count
-    )
+fn _format_note(timestamp: DateTime<Local>, note_value: &String) -> String {
+    format!("{} - {}", timestamp.to_rfc3339(), note_value)
+}
+
+fn search_notes(settings: Settings, search_term: &String, count: &u8) {
+    let file = get_notes_file(settings.notes_file_path);
+    let reader = RevLines::new(BufReader::new(file)).unwrap();
+    let matcher = SkimMatcherV2::default();
+
+    println!("Searching notes with term \"{}\"...", search_term);
+
+    let mut line_matches = Vec::new();
+    for line in reader {
+        let note = Note::new_from_line(&line);
+        let match_res = matcher.fuzzy_match(&note.note_value, search_term);
+        match match_res {
+            None => {}
+            Some(match_score) => line_matches.push((match_score, note)),
+        }
+    }
+
+    line_matches.sort_by_key(|a| a.0);
+    let print_count = min(*count, line_matches.len() as u8);
+
+    if settings.verbosity > 0 {
+        println!(
+            "Found {} matches, printing max {}.",
+            line_matches.len(),
+            print_count
+        );
+    }
+
+    for _ in 0..print_count {
+        let val = line_matches.pop().unwrap();
+        println!("{} (Score: {})", val.1, val.0)
+    }
+}
+
+fn echo_path(settings: Settings) {
+    println!("{}", settings.notes_file_path.display())
 }
 
 fn main() {
@@ -150,9 +231,10 @@ fn main() {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Some(Commands::Take { note_value }) => take_note(settings, note_value),
+        Some(Commands::Take { note_value, echo }) => take_note(settings, note_value, echo),
         Some(Commands::List { count }) => list_notes(settings, count),
         Some(Commands::Search { term, count }) => search_notes(settings, term, count),
+        Some(Commands::Path {}) => echo_path(settings),
         _ => {}
     }
 }
